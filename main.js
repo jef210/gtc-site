ee27b9342bc59da46ff9439e736bb1f45a88c542
jef210 <jmalek210@gmail.com>
2015-10-27 18:18:52 -0600

    added basic structure

---
+++ main.js
@@ -0,0 +1,76 @@
+(function(){
+
+$(document).ready(function(){
+    $('.nav li a').on('click', function(){
+    $('.navbar-toggle').click() 
+});
+
+    // Toggle the section content
+    
+    $('#data-extra-text').hide();
+    $('.data-section').on('click', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $('#data-extra-text').fadeToggle();
+    }); 
+
+    $('#teach-drum-building').hide();
+    $('.teach-drum-building').on('click', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $('#teach-drum-building').fadeToggle();
+    }); 
+
+    $('#drum-building-workshop').hide();
+    $('.drum-building-workshop').on('click', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $('#drum-building-workshop').fadeToggle();
+    }); 
+
+    $('#rhythmic-foundations').hide();
+    $('.rhythmic-foundations').on('click', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $('#rhythmic-foundations').fadeToggle();
+    });
+
+    $('#advanced-rhythmic-foundations').hide();
+    $('.advanced-rhythmic-foundations').on('click', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $('#advanced-rhythmic-foundations').fadeToggle();
+    });   
+
+    $('#digital-readers-theatre').hide();
+    $('.digital-readers-theatre').on('click', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $('#digital-readers-theatre').fadeToggle();
+    });
+    
+    $('#drum-circle-event').hide();
+    $('.drum-circle-event').on('click', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $('#drum-circle-event').fadeToggle();
+    });
+
+    $('#after-school-enrich').hide();
+    $('.after-school-enrich').on('click', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $('#after-school-enrich').fadeToggle();
+    });
+
+    $('#rhythmic-foundations-instructors').hide();
+    $('.rhythmic-foundations-instructors').on('click', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $('#rhythmic-foundations-instructors').fadeToggle();
+    });
+    // refactor to 1 function for all fadetoggles
+
+});
+
+})();
